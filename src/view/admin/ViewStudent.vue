daf014fbf08ab22c7d5181bf3c04a6c12b6b3bbc
Sandip Rajbhandari <srb.newar.yb0111@gmail.com>
2025-04-25 00:55:08 +0545

    register new user

--- src/view/admin/ViewStudent.vue
+++ src/view/admin/ViewStudent.vue
@@ -16,8 +16,8 @@ const showDialog = ref(false);
 const newUser = reactive({
   fullName: "",
   email: "",
-  passwords: null as string | null,
-  roleId: null as number | null,
+  passwords: "",
+  roleId: 2,
   status: true,
   address: {
     country: "",
@@ -35,12 +35,15 @@ async function handleSave() {
       roomId: selectRoom.value ? selectRoom.value.id : null,
     };
 
-    const response = await fetch("/api/register", {
-      method: "POST",
-      credentials: "include",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(payload),
-    });
+    const response = await fetch(
+      "http://localhost:8080/hostel_management_system_web/api/auth/register",
+      {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -111,16 +114,19 @@ function roomAction(row: any) {
         <div class="flex gap-2">
           <div>
             <input
+              v-model="newUser.fullName"
               type="text"
               class="w-full mb-3 p-3 h-9 border border-green-400 rounded-lg"
               placeholder="Enter Full Name"
             />
             <input
+              v-model="newUser.email"
               type="text"
               class="w-full mb-3 p-3 h-9 border border-green-400 rounded-lg"
               placeholder="Enter Full Email"
             />
             <input
+              v-model="newUser.passwords"
               type="password"
               class="w-full mb-3 p-3 h-9 border border-green-400 rounded-lg"
               placeholder="Enter Full Password"
@@ -136,21 +142,25 @@ function roomAction(row: any) {
 
           <div>
             <input
+              v-model="newUser.address.country"
               type="text"
               class="w-full mb-3 p-3 h-9 border border-green-400 rounded-lg"
               placeholder="Enter Country"
             />
             <input
+              v-model="newUser.address.district"
               type="text"
               class="w-full mb-3 p-3 h-9 border border-green-400 rounded-lg"
               placeholder="Enter District"
             />
             <input
+              v-model="newUser.address.rmcMc"
               type="text"
               class="w-full mb-3 p-3 h-9 border border-green-400 rounded-lg"
               placeholder="Enter Municipality"
             />
             <input
+              v-model="newUser.address.wardNo"
               type="number"
               min="1"
               class="w-full mb-3 p-3 h-9 border border-green-400 rounded-lg"
